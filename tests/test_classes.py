dc3f55c9742338da8d666c06cd52526202238ea5
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-03-23 13:36:14 +0100

    Linting

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -226,6 +226,7 @@ def test_get_config(expand):
     else:
         assert isinstance(config["slides"][0]["content"], str)  # not expanded
 
+
 @pytest.mark.parametrize("pdf_pages", ["all", [1, 2]])
 def test_pdf_pages_grouped_error(pdf_pages):
 
@@ -234,6 +235,7 @@ def test_pdf_pages_grouped_error(pdf_pages):
     with pytest.raises(ValueError):
         report.add_slide(grouped_content=[content_dir + "pdfs/multidogs_([0-9]).pdf"], pdf_pages=pdf_pages)
 
+
 @pytest.mark.parametrize("pdf_pages", ["all", 2, [1, 3], [2, 3, 3]])
 def test_pdf_pages_pass(pdf_pages):
 
@@ -241,6 +243,7 @@ def test_pdf_pages_pass(pdf_pages):
     tmp_files = report.convert_pdf(content_dir + "pdfs/multidogs.pdf", pdf_pages)
     for tmp_file in tmp_files:
         os.remove(tmp_file)
+
 
 @pytest.mark.parametrize("pdf_pages", [None, "h", -1, 0, 4])
 def test_index_pdf_pages_error(pdf_pages):
